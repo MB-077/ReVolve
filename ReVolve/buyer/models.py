f2168bdaf08773df97b4426a0f5c23aa4e46a296
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-06-02 21:03:33 +0530

    added seller's database

---
+++ ReVolve/buyer/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class buyer_need(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_material_type = models.CharField(max_length=100)
+    item_grade = models.CharField(max_length=50)
+    item_quantity = models.IntegerField(default=1,null=True)
+    item_volume = models.FloatField(null=True)
+
+    def __str__(self):
+      return f"{self.item_name}"
